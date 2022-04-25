31570c9585a083c89336db48c80887a941653884
chrisdlangton <chris@langton.cloud>
2022-04-26 00:47:08 +1000

    fix python-safety-dependencies-check

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+from pathlib import Path
 from setuptools import setup
+
+requirements = Path('requirements.txt')
+install_requires = []
+for line in requirements.read_text(encoding='utf8').splitlines():
+    req = line.strip()
+    if req.startswith('#'):
+        continue
+    install_requires.append(req)
 
 setup(
     name='trivialscan-api',
@@ -6,8 +15,6 @@ setup(
     author='Christopher Langton',
     author_email='chris@trivialsec.com',
     description='Validate the security of your TLS connections so that they deserve your trust.',
-    long_description="""
-""",
     long_description_content_type='text/markdown',
     url='https://gitlab.com/trivialsec/trivialscan-api',
     project_urls={
@@ -22,10 +29,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
     ],
     include_package_data=True,
-    install_requires=[
-        'trivialscan==2.3.1',
-        'validators==0.18.2'
-    ],
+    install_requires=install_requires,
     python_requires='>=3.9',
     options={'bdist_wheel': {'universal': '1'}},
 )
