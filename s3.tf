38f3b327fa84e898f5065ab3760f2ba0673379b0
chrisdlangton <chris@langton.cloud>
2022-04-26 00:15:42 +1000

    remove s3 public access

--- s3.tf
+++ s3.tf
@@ -4,9 +4,18 @@ resource "aws_s3_bucket" "trivialscan_bucket" {
     Name        = "cost-center"
     Environment = "rapidapi"
   }
+
 }
 
 resource "aws_s3_bucket_acl" "trivialscan_results_acl" {
   bucket = aws_s3_bucket.trivialscan_bucket.id
   acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.trivialscan_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
